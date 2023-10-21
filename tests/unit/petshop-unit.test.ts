1e21546bea229a25fbf657a4d89c550931dfbc6f
Cleiver <creibis10@gmail.com>
2023-10-21 11:50:46 -0300

    feat: unit tests for check the best petshop option

--- tests/unit/petshop-unit.test.ts
+++ tests/unit/petshop-unit.test.ts
@@ -7,31 +7,98 @@ describe("Petshop Service Unit Tests", () => {
 	  jest.clearAllMocks();
 	});
 
-	it("should return rentals", async () => {
+	it("should return all petshops", async () => {
 	  jest.spyOn(petshopRepository, "getPetshops").mockResolvedValueOnce([
 	    {...(buildPetshopInput()), id: 1, createdAt: new Date(), updatedAt: new Date()},
 	    {...(buildPetshopInput()), id: 2, createdAt: new Date(), updatedAt: new Date()},
     ]);
 	
-	  const rentals = await petshopService.getPetshops();
-	  expect(rentals).toHaveLength(2);
+	  const petshops = await petshopService.getPetshops();
+	  expect(petshops).toHaveLength(2);
 	});
 
-    it("should create a rental", async () => {
-        const petshop = buildPetshopInput();
-        jest.spyOn(petshopRepository, "createPetshop").mockResolvedValueOnce(
-          {...petshop, id: 1, createdAt: (new Date()), updatedAt: new Date()},
-      );
-      
-        const rentals = await petshopService.createPetshop(petshop);
-        expect(rentals).toEqual({
-            ...petshop, 
-            id: 1, 
-            createdAt: (new Date()), 
-            updatedAt: (new Date())
-        });
+  it("should create a petshop", async () => {
+      const petshop = buildPetshopInput();
+      jest.spyOn(petshopRepository, "createPetshop").mockResolvedValueOnce(
+        {...petshop, id: 1, createdAt: (new Date()), updatedAt: new Date()},
+    );
+    
+      const petshops = await petshopService.createPetshop(petshop);
+      expect(petshops).toEqual({
+          ...petshop, 
+          id: 1, 
+          createdAt: (new Date()), 
+          updatedAt: (new Date())
       });
+    });
 
+    it("should return the best petshop option", async () => {
+      const petshop = buildPetshopInput();
+      const pet1 = {
+        name: "teste1",
+        distance: 100, 
+        weekDayBigPrice: 10, 
+        weekDaySmallPrice: 10, 
+        weekEndBigPrice: 10, 
+        weekEndSmallPrice: 10, 
+        id: 1, 
+        createdAt: new Date(), 
+        updatedAt: new Date()
+      }
+
+      const pet2 = {
+        name: "teste2",
+        distance: 100, 
+        weekDayBigPrice: 20, 
+        weekDaySmallPrice: 20, 
+        weekEndBigPrice: 20, 
+        weekEndSmallPrice: 20, 
+        id: 2, 
+        createdAt: new Date(), 
+        updatedAt: new Date()
+      }
+      jest.spyOn(petshopRepository, "getPetshops").mockResolvedValueOnce([
+        pet1, pet2
+      ]);
+    
+      const petshops = await petshopService.checkBestPetshop({date: "21/10/2023", bigSizesCount: 1, smallSizesCount: 1});
+      expect(petshops).toEqual(pet1);
+    });
+
+    it("should return the closest petshop option if the price is a tie", async () => {
+      const petshop = buildPetshopInput();
+      const pet1 = {
+        name: "teste1",
+        distance: 500, 
+        weekDayBigPrice: 10, 
+        weekDaySmallPrice: 10, 
+        weekEndBigPrice: 10, 
+        weekEndSmallPrice: 10, 
+        id: 1, 
+        createdAt: new Date(), 
+        updatedAt: new Date()
+      }
+
+      const pet2 = {
+        name: "teste2",
+        distance: 100, 
+        weekDayBigPrice: 10, 
+        weekDaySmallPrice: 10, 
+        weekEndBigPrice: 10, 
+        weekEndSmallPrice: 10, 
+        id: 2, 
+        createdAt: new Date(), 
+        updatedAt: new Date()
+      }
+      jest.spyOn(petshopRepository, "getPetshops").mockResolvedValueOnce([
+        pet1, pet2
+      ]);
+    
+      const petshops = await petshopService.checkBestPetshop({date: "21/10/2023", bigSizesCount: 1, smallSizesCount: 1});
+      expect(petshops).toEqual(pet2);
+    });
+
+    
 })
 
 describe("Tests", () => {
