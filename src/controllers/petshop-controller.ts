16e2973af5c6b18162313d5dcc3e46648a1ecb16
Cleiver <creibis10@gmail.com>
2023-10-21 21:04:59 -0300

    feat: not integers excpetion for check best petshop input

--- src/controllers/petshop-controller.ts
+++ src/controllers/petshop-controller.ts
@@ -2,6 +2,7 @@ import { NextFunction, Request, Response } from "express";
 import httpStatus from "http-status";
 import { BestPetshop, CreatePetshop } from "../protocols";
 import petshopService from "../services/petshop-service";
+import { badRequestError } from "../errors/badresquest-error";
 
 export async function createPetshop(req: Request, res: Response, next: NextFunction) {
   const petshopInput = req.body as CreatePetshop;
@@ -29,11 +30,12 @@ export async function checkBestPetshop(req: Request, res: Response, next: NextFu
   const petDayInfos = req.body as BestPetshop;
 
   try {
+    if((petDayInfos.bigSizesCount) / Math.floor(petDayInfos.bigSizesCount) !== 0) throw badRequestError("'bigSizesCount' must be a integer")
+    if((petDayInfos.smallSizesCount) / Math.floor(petDayInfos.smallSizesCount) !== 0) throw badRequestError("'smallSizesCount' must be a integer")
+
     const response = await petshopService.checkBestPetshop(petDayInfos);
     res.send(response).status(httpStatus.OK);
   } catch (error) {
-    // next(error);
-    // all erros are covered
-    // integration covered teste complain about this line
+    next(error);
   }
 }
